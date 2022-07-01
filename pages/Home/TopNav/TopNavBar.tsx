646b306af0c299a3fea3f914f99e2b53411b9997
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-07-01 22:39:46 +0100

    working on the topnavbar

--- pages/Home/TopNav/TopNavBar.tsx
+++ pages/Home/TopNav/TopNavBar.tsx
@@ -3,38 +3,87 @@ import * as UI from "../../../components"
 
 
 
+
+const Cart = () => {
+    return (
+        <div className='flex justify-between items-center'>
+           {/* cart */}
+              <div className="cart">
+                    <img src="/assets/cart.png" alt="cart" />
+                    <div className="cart-count">
+                        <span>0</span>
+                    </div>
+               </div>         
+        </div>
+    )
+}
+
+
 const TopNavRightBar = () => {
     return (
         <div className='w-[70%] border-2 border-[#d7d7d7]'>
-            <ul className="topest--header flex">
-                <li className="currency">
-                     <UI.DropDown
-                        apperance={true}
-                        className="bg-white"
-                        items={[
-                            {text: 'USD', value: 'USD'},
-                            {text: 'EUR', value: 'EUR'},
-                        ]}
-                     />
-                </li>
-                <li className="language">
-                    <UI.DropDown
-                        apperance={true}
-                        className="bg-white"
-                        items={[
-                            {text: 'English', value: 'English'},
-                            {text: 'Deutsch', value: 'Deutsch'},
-                        ]}
-                    />
-                </li>
-                <li className="welcome-msg">
-                    <UI.Paragraphs
-                        text="Welcome to our store"
-                        fontSize="text-sm"
-                        color="text-black"
-                    />
-                </li>
-            </ul>
+            <div className="list-items">
+                <ul className="topest--header flex">
+                    <li className="currency">
+                        <UI.DropDown
+                            apperance={true}
+                            className="bg-white"
+                            items={[
+                                {text: 'USD', value: 'USD'},
+                                {text: 'EUR', value: 'EUR'},
+                            ]}
+                        />
+                    </li>
+                    <li className="language">
+                        <UI.DropDown
+                            apperance={true}
+                            className="bg-white"
+                            items={[
+                                {text: 'English', value: 'English'},
+                                {text: 'Deutsch', value: 'Deutsch'},
+                                {text: 'Français', value: 'Français'},
+                            ]}
+                        />
+                    </li>
+                    <li className="welcome-msg">
+                        <UI.Paragraphs
+                            text="Welcome to our store"
+                            fontSize="text-sm"
+                            color="text-black"
+                        />
+                    </li>
+                </ul>
+                <ul className="topest--header flex">
+                    <li className="currency">
+                        <UI.Paragraphs
+                            text="Compare Products"
+                            fontSize="text-sm"
+                            color="text-black"
+                        />
+                    </li>
+                    <li className="language">
+                       <UI.Paragraphs
+                            text="My Account"
+                            fontSize="text-sm"
+                            color="text-black"
+                        />
+                    </li>
+                    <li className="welcome-msg">
+                        <UI.Paragraphs
+                            text="My Wishlist"
+                            fontSize="text-sm"
+                            color="text-black"
+                        />
+                    </li>
+                    <li className="welcome-msg">
+                        <UI.Paragraphs
+                            text="Sign In"
+                            fontSize="text-sm"
+                            color="text-black"
+                        />
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
