8df0bbe61b0805fbd7eca775cc583fd4e17767eb
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-07-03 07:30:14 +0100

    working on page navbar

--- layouts/Home/Navbar/Navbar.tsx
+++ layouts/Home/Navbar/Navbar.tsx
@@ -3,12 +3,31 @@ import React from 'react'
 const Navbar:React.FC = () => {
 
 
+    const navbarItems=[
+        {
+            name:"Home",
+            link:"/"
+        },{
+            name:"/Mercedes",
+            link:"/mercedes"
+        },{
+            name:"/bmw",
+        },{
+            name:"Huyndai",
+        }
+    ]
+
+
   return (
-    <div>Navbar</div>
+    <div className=''>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+
+        </nav>
+    </div>
   )
 
 
-  
+
 }
 
 export default Navbar
