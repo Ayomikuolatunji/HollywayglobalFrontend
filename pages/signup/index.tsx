bb18a698aeb01ec2f8b61260003e5ce769f536ae
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-07-03 15:19:08 +0100

    working on user signup page

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -4,22 +4,26 @@ import * as UI from "../../components"
 
 const Signup = () => {
   return (
-    <div className='bg-white mt-4 border-[1px] border-[#d7d7d7]'>
-         <div className='flex justify-between items-center w-[100%]'>
-              <div className="personal-information w-[50%] p-3">
+    <div className='bg-white mt-4 border-[1px] border-[#d7d7d7] h-auto relative'>
+         <div className='flex items-center w-[100%]'>
+              <div className="personal-information w-[50%] px-3 h-auto">
                   <div className="title">
                       <h1 className='text-black text-2xl font-extrabold'>
                         Personal Information
                       </h1>
                   </div>
                   <div className="form w-[100%]">
-                    <div className="first-name flex mt-5">
-                        <label className='text-[#69686c] font-bold'>First Name</label>
-                        <span className='text-red-500 text-xl ml-4 mb-5'>*</span>
-                        <UI.InputField
-                          type="text"
-                          className="first-name ml-12 bg-white border-[1px] border-[#dd] outline-none p-1 w-[100%]"
-                        />
+                    <div className="first-name flex mt-5 items-center">
+                        <div>
+                          <label className='text-[#69686c] font-bold'>First Name</label>
+                          <span className='text-red-500 text-xl ml-4 mb-5'>*</span>
+                        </div>
+                        <div>
+                          <UI.InputField
+                            type="text"
+                            className="first-name ml-12 bg-white border-[1px] border-[#dd] outline-none p-1 w-[100%]"
+                          />
+                        </div>
                     </div>
                     <div className="last-name flex mt-5 w-[100%]">
                         <label className='text-[#69686c] font-bold'>Last Name</label>
@@ -38,15 +42,10 @@ const Signup = () => {
                           Sign Up for Newsletter
                         </label>
                     </div>
-                    <div className="create-account-btn mt-24">
-                       <UI.Button 
-                        className="text-white font-bold py-2 px-4 ml-36 bg-red-color"
-                        text="Create Account"
-                       />
-                    </div>
+                  
                   </div>
               </div>
-              <div className="left-login-information w-[50%]">
+              <div className="left-login-information w-[50%] h-auto block px-3">
                     <div className="form w-[100%]">
                       <div className="title">
                           <h1 className='text-black text-2xl font-extrabold'>
@@ -80,6 +79,15 @@ const Signup = () => {
                   </div>
               </div>
          </div>
+         <div className="create-account-btn mt-12 mb-9 text-center">
+              <UI.Button 
+              className="text-white py-2 px-4 -ml-44 bg-red-color hover:bg-gray-500 transition-[background-color] duration-500 ease-in-out font-[600]"
+              text="Create Account"
+              />
+         </div>
+         <span className='absolute bottom-0'>
+             Home
+         </span>
     </div>
   )
 }
