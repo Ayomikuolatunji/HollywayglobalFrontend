fa1bb1ecf25d6cec94ec1e461b7a4789d2ca1435
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-07-08 22:33:13 +0100

    working on carousel slider

--- layouts/Home/Carousel/Carousel.tsx
+++ layouts/Home/Carousel/Carousel.tsx
@@ -1,17 +1,37 @@
 import React from 'react'
 import Slider from "react-slick";
-import "~slick-carousel/slick/slick.css"; 
-import "~slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+
 
 
 
 
 export default function Carosel() {
 
-    
-  return (
-    <div className='border-2 w-[100%]'>
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+  };
 
+
+  return (
+    <div className='border-2 w-[100%] block relative h-[40vh]'>
+        <Slider {...settings}>
+          <div className='h-[50vh] max-w-full w-full'>
+              <img src="/assets/bg4-slider.jpg" alt="slider-img" />
+          </div>
+          <div className='h-[50vh] max-w-full w-full'>
+              <img src="/assets/bg5-slider.jpg" alt="slider-img" />
+          </div>
+        </Slider>
     </div>
   )
 }
