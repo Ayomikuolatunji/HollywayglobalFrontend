0c61d525c6d890e00f58abecd94f4a2889e8698d
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-07-08 20:07:05 +0100

    sidebar category

--- helpers/navItems.tsx
+++ helpers/navItems.tsx
@@ -1,113 +1,113 @@
-import {AiOutlinePlus,AiOutlineMinus} from "react-icons/ai";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
-const navbarItems=[
-        {
-            name:"Home",
-            link:"/",
-        },{
-            name:"Mercedes",
-            link:"/mercedes",
-            openIcon:<AiOutlinePlus/>,
-            closeIcon:<AiOutlineMinus/>,
-            subNav:[
-                {
-                    name:"Spear Part",
-                    link:"/mercedes/spear-part",
-                },
-                {
-                    name:"Engines",
-                    link:"/mercedes/mercedes-benz/engines",
-                },{
-                    name:"Transmissions",
-                    link:"/mercedes/mercedes-benz/transmissions",
-                }
-            ]
-        },{
-            name:"Bmw",
-            link:"/bmw",
-            openIcon:<AiOutlinePlus/>,
-            closeIcon:<AiOutlineMinus/>,
-            subNav:[
-                {
-                    name:"Spear Part",
-                    link:"/bmw/spear-part",
-                },
-                {
-                    name:"Engines",
-                    link:"/bmw/engines",
-                }
-            ]
-        },{
-            name:"Huyndai",
-            link:"/huyndai",
-            openIcon:<AiOutlinePlus/>,
-            closeIcon:<AiOutlineMinus/>,
-            subNav:[
-                {
-                    name:"Spear Part",
-                    link:"/huyndai/spear-part",
-                },
-                {
-                    name:"Engines",
-                    link:"/huyndai/engines",
-                }
-            ]
-        },{
-            name:"Toyota",
-            link:"/toyota",
-            openIcon:<AiOutlinePlus/>,
-            closeIcon:<AiOutlineMinus/>,
-            subNav:[
-                {
-                    name:"Spear Part",
-                    link:"/toyota/spear-part",
-                },{
-                    name:"Engines",
-                    link:"/toyota/engines",
-                }
-            ]
+const navbarItems = [
+    {
+        name: "Home",
+        link: "/",
+    }, {
+        name: "Mercedes",
+        link: "/mercedes",
+        openIcon: <AiOutlinePlus />,
+        closeIcon: <AiOutlineMinus />,
+        subNav: [
+            {
+                name: "Spear Part",
+                link: "/mercedes/spear-part",
+            },
+            {
+                name: "Engines",
+                link: "/mercedes/mercedes-benz/engines",
+            }, {
+                name: "Transmissions",
+                link: "/mercedes/mercedes-benz/transmissions",
+            }
+        ]
+    }, {
+        name: "Bmw",
+        link: "/bmw",
+        openIcon: <AiOutlinePlus />,
+        closeIcon: <AiOutlineMinus />,
+        subNav: [
+            {
+                name: "Spear Part",
+                link: "/bmw/spear-part",
+            },
+            {
+                name: "Engines",
+                link: "/bmw/engines",
+            }
+        ]
+    }, {
+        name: "Huyndai",
+        link: "/huyndai",
+        openIcon: <AiOutlinePlus />,
+        closeIcon: <AiOutlineMinus />,
+        subNav: [
+            {
+                name: "Spear Part",
+                link: "/huyndai/spear-part",
+            },
+            {
+                name: "Engines",
+                link: "/huyndai/engines",
+            }
+        ]
+    }, {
+        name: "Toyota",
+        link: "/toyota",
+        openIcon: <AiOutlinePlus />,
+        closeIcon: <AiOutlineMinus />,
+        subNav: [
+            {
+                name: "Spear Part",
+                link: "/toyota/spear-part",
+            }, {
+                name: "Engines",
+                link: "/toyota/engines",
+            }
+        ]
 
-        },{
-            name:"Lamborghini",
-            link:"/lamborghini",
-            openIcon:<AiOutlinePlus/>,
-            closeIcon:<AiOutlineMinus/>,
-            subNav:[
-                {
-                    name:"Spear Part",
-                    link:"/lamborghini/spear-part",
+    }, {
+        name: "Lamborghini",
+        link: "/lamborghini",
+        openIcon: <AiOutlinePlus />,
+        closeIcon: <AiOutlineMinus />,
+        subNav: [
+            {
+                name: "Spear Part",
+                link: "/lamborghini/spear-part",
 
-                }
-            ]
-        },{
-            name:"Ferrari",
-            link:"/ferrari",
-            openIcon:<AiOutlinePlus/>,
-            closeIcon:<AiOutlineMinus/>,
-            subNav:[
-                {
-                    name:"Spear Part",
-                    link:"/ferrari/spear-part",
-                },{
-                    name:"Engines",
-                    link:"/ferrari/engines",
-                }
-            ]
-        },{
-            name:"Porsche",
-            link:"/porsche",
-            openIcon:<AiOutlinePlus/>,
-            closeIcon:<AiOutlineMinus/>,
-            subNav:[
-                {
-                    name:"Spear Part",
-                    link:"/porsche/spear-part",
-                },{
-                    name:"Engines",
-                    link:"/porsche/engines",
-                }
-            ]
-        }
+            }
+        ]
+    }, {
+        name: "Ferrari",
+        link: "/ferrari",
+        openIcon: <AiOutlinePlus />,
+        closeIcon: <AiOutlineMinus />,
+        subNav: [
+            {
+                name: "Spear Part",
+                link: "/ferrari/spear-part",
+            }, {
+                name: "Engines",
+                link: "/ferrari/engines",
+            }
+        ]
+    }, {
+        name: "Porsche",
+        link: "/porsche",
+        openIcon: <AiOutlinePlus />,
+        closeIcon: <AiOutlineMinus />,
+        subNav: [
+            {
+                name: "Spear Part",
+                link: "/porsche/spear-part",
+            }, {
+                name: "Engines",
+                link: "/porsche/engines",
+            }
+        ]
+    }
 ]
 
 
--- layouts/wrapper/SideBar/components/Category.tsx
+++ layouts/wrapper/SideBar/components/Category.tsx
@@ -4,7 +4,7 @@ import { navItems } from "../../../../helpers"
 
 export default function Category() {
     return (
-        <div className='border-8 w-[95%] mt-5 border-white shadow-lg'>
+        <div className='border-2 w-[95%] mt-5 border-[lightgray] shadow-lg'>
             <div className="title bg-black text-white p-3 border-2">
                 <h3 className='font-[600] text-[16px] font-sans'>Category</h3>
             </div>
@@ -12,7 +12,7 @@ export default function Category() {
                 {
                     navItems.map((item, index) => (
                         <Link href={item.link}>
-                            <li key={index} className="flex justify-between items-center first:hidden p-3 border-[1px] border-[#bdbbbb]">
+                            <li key={index} className="flex justify-between items-center first:hidden p-3 border-b-[1px] border-[#bdbbbb]">
                                 <a>{item.name}</a>
                                 <span>
                                     {item.openIcon}
