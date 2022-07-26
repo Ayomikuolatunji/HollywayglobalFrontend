0a3cc008924b902937d9195de755e346512f323f
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-07-26 21:01:15 +0100

    using rtk for user signup form

--- redux/apis/authApi.tsx
+++ redux/apis/authApi.tsx
@@ -1,18 +1,34 @@
-import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
-import { loginCredentails } from "../../models/authTypings"
-import { apiService } from "../service"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { loginCredentails } from "../../models/authTypings";
+import { apiService } from "../service";
 
+interface signupTypings {
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+}
 
 export const authApi = apiService.injectEndpoints({
-    endpoints:build=>({
-     login:build.mutation<void, loginCredentails>({
-          query:(credentials)=>({
-               url:"/login",
-               method:"POST",
-               body:credentials
-          })
-     })
-    })
-})
+  endpoints: (build) => ({
+    login: build.mutation<void, loginCredentails>({
+      query: (credentials) => ({
+        url: "/login",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    signup: build.mutation<void, signupTypings>({
+      query: (body) => {
+        return {
+          url: "/signup",
+          method: "POST",
+          body,
+        };
+      },
+    }),
+  }),
+});
 
-export const { useLoginMutation } =authApi
+export const { useLoginMutation, useSignupMutation } = authApi;
