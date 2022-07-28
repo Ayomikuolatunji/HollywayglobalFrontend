92fc6b51f594b227d1d3fdd36fef25095cd1fc00
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-07-28 22:25:17 +0100

    admin dashboard layout

--- components/AdminWrapper/AdminSidebar.tsx
+++ components/AdminWrapper/AdminSidebar.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import { RiGroupFill } from "react-icons/ri";
+import { AiFillTags } from "react-icons/ai";
+import { FaMoneyBill } from "react-icons/fa";
+import Link from "next/link";
+
+export const sideBarItems = [
+  {
+    name: "Dashboard",
+    icon: <RiGroupFill className="text-xl" />,
+    path: "/admin-dashboard",
+  },
+  {
+    name: "Products",
+    icon: <RiGroupFill className="text-xl" />,
+    path: "/products",
+  },
+  {
+    name: "Categories",
+    icon: <AiFillTags className="text-xl" />,
+    path: "/categories",
+  },{
+    name: "Orders",
+    icon: <FaMoneyBill className="text-xl" />,
+    path: "/orders",
+  },
+  {
+    name: "Users",
+    icon: <FaMoneyBill className="text-xl" />,
+    path: "/users",
+  },
+  {
+    name: "Transactions",
+    icon: <FaMoneyBill className="text-xl" />,
+    path: "/transactions",
+  },
+];
 
 function AdminSidebar() {
   return (
-    <div>AdminSidebar</div>
-  )
+    <div className="w-full h-[100vh] bg-[#FFFFFF] shadow-sidebar-Shadow border-r-gray-100 border-r-4 sticky top-0 left-0 bottom-0">
+      <div className="logo p-4 flex justify-center">
+        <img src="/assets/logo.png" alt="logo" />
+      </div>
+      {/* sidebar items */}
+      <div className="sidebar-items p-4 flex justify-center flex-col mt-10">
+        <ul className="w-[80%] mx-auto flex justify-center flex-col">
+          {sideBarItems.map((item, index) => {
+            return (
+              <Link href={item.path} key={index}>
+                <div className="flex items-center pl-7 p-2 nav-items mt-3 py- ml-3 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-[#F2FAFF] dark:hover:bg-gray-700">
+                  <span>{item.icon}</span>
+                  <span className="sidebar-item-name p-2 text-xl">
+                    {item.name}
+                  </span>
+                </div>
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default AdminSidebar
+export default AdminSidebar;
