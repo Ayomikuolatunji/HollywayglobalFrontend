6867961a390cf3e4c3a308856f2d6b4d02b2cc3c
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-07-28 23:14:39 +0100

    admin dashboard products tables

--- helpers/index.tsx
+++ helpers/index.tsx
@@ -1,3 +1,4 @@
 export { default as navItems } from './navItems'
 export { default as isAuthenticated } from "./Auth"
 export {default as Toastify} from "./toastify"
+export {default as FakeProduct} from "./FakeProduct"
