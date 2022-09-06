c114acdd90d762e8d23fe52896b529110a98d5a8
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-06 20:39:24 +0100

    user navbar

--- helpers/navItems.tsx
+++ helpers/navItems.tsx
@@ -5,69 +5,69 @@ const navbarItems = [
         link: "/",
         subNav: []
     }, {
-        name: "Mercedes",
-        link: "/mercedes",
+        name: "Local Products",
+        link: "/local-products",
         subNav: [
-            {
-                name: "Spear Part",
-                link: "/mercedes/spear-part",
-            },
-            {
-                name: "Engines",
-                link: "/mercedes/mercedes-benz/engines",
-            }, {
-                name: "Transmissions",
-                link: "/mercedes/mercedes-benz/transmissions",
-            }
+            // {
+            //     name: "Spear Part",
+            //     link: "/mercedes/spear-part",
+            // },
+            // {
+            //     name: "Engines",
+            //     link: "/mercedes/mercedes-benz/engines",
+            // }, {
+            //     name: "Transmissions",
+            //     link: "/mercedes/mercedes-benz/transmissions",
+            // }
         ]
     }, {
-        name: "Bmw",
-        link: "/bmw",
+        name: "Exports",
+        link: "/exports",
         subNav: [
-            {
-                name: "Spear Part",
-                link: "/bmw/spear-part",
-            },
-            {
-                name: "Engines",
-                link: "/bmw/engines",
-            }
+            // {
+            //     name: "Spear Part",
+            //     link: "/bmw/spear-part",
+            // },
+            // {
+            //     name: "Engines",
+            //     link: "/bmw/engines",
+            // }
         ]
     }, {
-        name: "Huyndai",
-        link: "/huyndai",
+        name: "Irrigation Material",
+        link: "/irrigation-material",
         subNav: [
-            {
-                name: "Spear Part",
-                link: "/huyndai/spear-part",
-            },
-            {
-                name: "Engines",
-                link: "/huyndai/engines",
-            }
+            // {
+            //     name: "Spear Part",
+            //     link: "/huyndai/spear-part",
+            // },
+            // {
+            //     name: "Engines",
+            //     link: "/huyndai/engines",
+            // }
         ]
     }, {
-        name: "Toyota",
-        link: "/toyota",
+        name: "Mineral Rescources",
+        link: "/mineral-rescources",
         subNav: [
-            {
-                name: "Spear Part",
-                link: "/toyota/spear-part",
-            }, {
-                name: "Engines",
-                link: "/toyota/engines",
-            }
+            // {
+            //     name: "Spear Part",
+            //     link: "/toyota/spear-part",
+            // }, {
+            //     name: "Engines",
+            //     link: "/toyota/engines",
+            // }
         ]
 
     }, {
         name: "Lamborghini",
         link: "/lamborghini",
         subNav: [
-            {
-                name: "Spear Part",
-                link: "/lamborghini/spear-part",
+            // {
+            //     name: "Spear Part",
+            //     link: "/lamborghini/spear-part",
 
-            }
+            // }
         ]
     }, {
         name: "Ferrari",
