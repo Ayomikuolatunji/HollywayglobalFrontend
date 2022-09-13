fcea407d6d9360116948cd5de40ddfd04aaf10a5
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-13 21:20:37 +0100

    env varibale and production endpoints

--- redux/service.tsx
+++ redux/service.tsx
@@ -6,7 +6,10 @@ const admin = getAppCredentials("admin_token");
 export const apiService = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/api/v1/",
+    baseUrl:
+      process.env.NODE_ENV === "production"
+        ? "https://myql-learn-db.herokuapp.com/api/v1/"
+        : "http://localhost:8080/api/v1/",
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
       headers.set("Accept", "application/json");
@@ -19,8 +22,11 @@ export const apiService = createApi({
 
 export const secureApiService = createApi({
   reducerPath: "secureApi",
-   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/api/v1/",
+  baseQuery: fetchBaseQuery({
+    baseUrl:
+      process.env.NODE_ENV == "production"
+        ? "https://myql-learn-db.herokuapp.com/api/v1/"
+        : "http://localhost:8080/api/v1/",
     prepareHeaders: (headers) => {
       if (admin?.admin_id && admin.admin_token) {
         headers.set("Content-Type", "application/json");
