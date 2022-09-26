1957678b17981c7dc01beab5249c897853199bed
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-26 19:28:27 +0100

    our ecommerce

--- components/Wrapper/TopNav/TopNavBar.tsx
+++ components/Wrapper/TopNav/TopNavBar.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+import {
+  AiOutlineMail,
+  AiFillFacebook,
+  AiOutlineTwitter,
+  AiFillLinkedin,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
 
 export default function TopNavBar() {
   return (
-    <div>TopNavBar</div>
-  )
+    <div>
+      <div className="left">
+        <ul>
+          <li>
+            <AiOutlineMail />
+            <span>hello@colorlib.com</span>
+          </li>
+          <li>
+            <span>Free Shipping for all Order of $99</span>
+          </li>
+        </ul>
+      </div>
+      <div className="right">
+        <div className="icons">
+          <AiFillFacebook />
+          <AiOutlineTwitter />
+          <AiFillLinkedin />
+          <AiOutlineWhatsApp />
+        </div>
+      </div>
+    </div>
+  );
 }
