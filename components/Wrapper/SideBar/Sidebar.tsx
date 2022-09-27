0a52b5bc10686405341db874c77a3bc5b56b92cf
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-27 17:01:11 +0100

    done with sidebar component and styles

--- components/Wrapper/SideBar/Sidebar.tsx
+++ components/Wrapper/SideBar/Sidebar.tsx
@@ -14,17 +14,27 @@ export default function Sidebar() {
       <Popover className="mt-10 w-full">
         {({ open }) => (
           <>
-            <Popover.Button className="flex items-center space-x-4 py-3 px-5 bg-[#7fad39] text-white hover:border-0 hover:outline-0">
-              <GiHamburgerMenu />
-              <span>All Departments</span>
-              <BsChevronDown
-                className={`${open ? "rotate-180 transform" : ""}text-white`}
-              />
+            <Popover.Button className="flex items-center space-x-4 py-3 px-5 bg-[#7fad39] outline-none text-white font-bold hover:border-0 hover:outline-0 w-full">
+              <GiHamburgerMenu className="ml-3" />
+              <span className="text-lg">All Departments</span>
+              <BsChevronDown className={`text-white ml-4`} />
             </Popover.Button>
-            <Popover.Panel className="flex flex-col">
+            <Popover.Panel
+              static
+              className="flex flex-col space-y-5 border-2 px-3 py-3"
+            >
+              <a href="/insights" className="mt-3">
+                Insights
+              </a>
+              <a href="/automations">Automations</a>
+              <a href="/reports">Reports</a>
               <a href="/insights">Insights</a>
               <a href="/automations">Automations</a>
               <a href="/reports">Reports</a>
+              <a href="/insights">Insights</a>
+              <a href="/automations">Automations</a>
+              <a href="/reports">Reports</a>
+              <a href="/insights">Insights</a>
             </Popover.Panel>
           </>
         )}
