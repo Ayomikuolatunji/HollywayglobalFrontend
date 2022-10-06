f6149be63431d793b593322cd617e2ad7c19da49
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-10-06 17:17:18 +0100

    fix sidebar content issue

--- helpers/index.tsx
+++ helpers/index.tsx
@@ -1,4 +1,3 @@
 export { default as navItems } from './navItems'
 export { default as isAuthenticated } from "./Auth"
 export {default as Toastify} from "./toastify"
-export {default as FakeProduct} from "./FakeProduct"
