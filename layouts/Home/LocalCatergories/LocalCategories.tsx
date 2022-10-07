a5c973963d2d7d72c8fca20f8310975a0c4cdff7
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-10-08 00:14:37 +0100

    working with local products tabs

--- layouts/Home/LocalCatergories/LocalCategories.tsx
+++ layouts/Home/LocalCatergories/LocalCategories.tsx
@@ -6,17 +6,25 @@ export default function LocalCategories() {
   return (
     <div className="my-10">
       <div className="title text-center my-10">
-        <h1>Local Products</h1>
+        <h1 className="text-[#1c1c1c] relative font-[700] text-[26px] after:">
+          Local Products
+        </h1>
       </div>
       <Tabs
-        Tabheaders={["Popular Foods","Fruit & Vegetables", "Poutry & Seafoods", "Tab 3"]}
+        Tabheaders={[
+          "Popular Foods",
+          "Fruit & Vegetables",
+          "Poutry & Seafoods",
+          "Tab 3",
+        ]}
         tab={true}
         renderTabPanel={() => (
           <Tab.Panels>
             <Tab.Panel>Content 1</Tab.Panel>
             <Tab.Panel>Content 2</Tab.Panel>
             <Tab.Panel>Content 3</Tab.Panel>
-            <Tab.Panel>Content 3</Tab.Panel>
+            <Tab.Panel>Content 4</Tab.Panel>
+            <Tab.Panel>Content 5</Tab.Panel>
           </Tab.Panels>
         )}
       />
