dbbc35f93eecc48ef117e300182ee994fe4b76de
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-10-13 21:08:07 +0100

    rebuilding admin login

--- pages/admin-login/index.tsx
+++ pages/admin-login/index.tsx
@@ -87,99 +87,53 @@ const AdminLogin = () => {
   };
 
   return (
-    <form className="bg-white mt-4 border-[1px] border-[#d7d7d7] h-auto relative">
-      <div className="flex w-[100%]">
-        <div className="personal-information w-[50%] p-3">
-          <div className="title">
-            <h1 className="text-black text-lg font-extrabold">Admin Login</h1>
-
-            <h3 className="mt-10">
-              If you have an account, sign in with your email address
-            </h3>
+    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="w-full max-w-md space-y-8">
+      <div>
+        <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/>
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+        <p className="mt-2 text-center text-sm text-gray-600">
+          Or
+          <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
+        </p>
+      </div>
+      <form className="mt-8 space-y-6" action="#" method="POST">
+        <input type="hidden" name="remember" value="true"/>
+        <div className="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label htmlFor="email-address" className="sr-only">Email address</label>
+            <input id="email-address" name="email" type="email" autoComplete="email" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address"/>
           </div>
-          <div className="form w-[100%]">
-            <div className="first-name flex mt-5 items-center">
-              <div>
-                <label className="text-[#69686c] font-bold">Email</label>
-                <span className="text-red-500 text-xl ml-4 mb-5">*</span>
-              </div>
-              <div className="ml-12">
-                <UI.InputField
-                  type="email"
-                  className="email bg-white border-[1px] border-[#dd] outline-none p-1 w-[100%]"
-                  isHookForm={true}
-                  label="email"
-                  required={true}
-                  register={register}
-                />
-                <span className="mt-1">This is a required field.</span>
-              </div>
-            </div>
-            <div className="password flex mt-5 w-[100%] items-center">
-              <div>
-                <label className="text-[#69686c] font-bold">Password</label>
-                <span className="text-red-500 text-xl ml-4 mb-1">*</span>
-              </div>
-              <div className="ml-12">
-                <UI.InputField
-                  type="text"
-                  className="last-name  bg-white border-[1px] border-[#dd] outline-none p-1 w-[100%]"
-                  isHookForm={true}
-                  label="password"
-                  register={register}
-                  required={true}
-                />
-                <span className="mt-1">This is a required field.</span>
-              </div>
-            </div>
-            <div className="newletter flex mt-6 w-[100%] ml-36 items-center">
-              <div>
-                <input
-                  type="checkbox"
-                  className="last-name bg-white border-[1px] border-[#dd] outline-none p-1 w-[100%]"
-                  onChange={() => setRememberMe(!rememberMe)}
-                  checked={rememberMe}
-                />
-              </div>
-              <div>
-                <label className="text-[#69686c] font-normal ml-3">
-                  Remember me on this device
-                </label>
-              </div>
-            </div>
+          <div>
+            <label htmlFor="password" className="sr-only">Password</label>
+            <input id="password" name="password" type="password" autoComplete="current-password" required className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password"/>
           </div>
         </div>
-        <div className="left-login-information w-[50%] block p-3">
-          <div className="title">
-            <h1 className="text-black text-lg font-extrabold">New Admin</h1>
-            <h3 className="mt-5">
-              Creating an account has many benefits: check out faster, keep more
-              than one address, track orders and more.
-            </h3>
+  
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"/>
+            <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">Remember me</label>
           </div>
-          <div className="create-account-btn mt-12 mb-9 text-center">
-            <Link href={"/admin-signup"} passHref>
-              <a className="text-white py-2 px-4 -ml-44 bg-red-color hover:bg-gray-500 transition-[background-color] duration-500 ease-in-out font-[600]">
-                CREATE AN ACCOUNT
-              </a>
-            </Link>
+  
+          <div className="text-sm">
+            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
           </div>
         </div>
-      </div>
-      <div className="create-account-btn mt-12 mb-9 text-center">
-        <UI.Button
-          className="text-white py-2 px-4 -ml-44 bg-red-color hover:bg-gray-500 transition-[background-color] duration-500 ease-in-out font-[600]"
-          text="SIGN IN"
-          type="submit"
-          onClick={handleSubmit(onSubmit)}
-        />
-      </div>
-      <div className="error text-center">
-        <span className="text-red-500 text-xl">
-          {errors.email?.type && "All fields are required"}
-        </span>
-      </div>
-    </form>
+  
+        <div>
+          <button type="submit" className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+              </svg>
+            </span>
+            Sign in
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
   );
 };
 
