2ef09761758a3ece429037559ced218ae75a9a96
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-12-09 21:21:57 +0100

    product detail reviews

--- components/DetailPageComponents/DetailsPageReviews.tsx
+++ components/DetailPageComponents/DetailsPageReviews.tsx
@@ -1,19 +1,32 @@
 import { Tab } from "@headlessui/react";
+import { Fragment } from "react";
+
+const tabListsTitles = ["Description", "Information", "Reviews"];
 
 export default function DetailsPageReviews() {
   return (
-    <div className="mt-24 mb-6 w-full">
+    <div className="mt-24 mb-6 w-full relative">
       <Tab.Group manual>
-        <Tab.List className="flex justify-center space-x-6">
-          <Tab>Description</Tab>
-          <Tab>Information</Tab>
-          <Tab>Reviews</Tab>
+        <Tab.List className="flex justify-center space-x-16 mb-8 before:w-[35%] before:absolute before:left-0 before:h-[1px] before:top-2.5 before:bg-[#ebebeb]  before:contents('')  after:w-[35%] after:absolute after:right-0 after:h-[1px] after:top-2.5 after:bg-[#ebebeb]  after:contents('')">
+          {tabListsTitles.map((tab, index) => (
+            <Tab as={Fragment} key={index}>
+              {({ selected }) => (
+                <h1
+                  className={`${
+                    selected ? " text-black text-[18px] outline-none font-extrabold" : "text-gray-500"
+                  } cursor-pointer hover:border-0`}
+                >
+                  {tab}
+                </h1>
+              )}
+            </Tab>
+          ))}
         </Tab.List>
         <Tab.Panels>
           <Tab.Panel>
             <div>
               <h1 className="mb-[26px] font-[700] text-[#333333]">
-                Product Descrip
+                Product Description
               </h1>
               <p>
                 Vestibulum ac diam sit amet quam vehicula elementum sed sit amet
@@ -50,10 +63,14 @@ export default function DetailsPageReviews() {
                 Submit your review
               </h1>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos
-                molestiae libero sint ab iste obcaecati quis laboriosam. Maiores
-                ab perspiciatis iure? Qui laborum cum mollitia dicta incidunt
-                quis explicabo delectus?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+                sint ullam recusandae perspiciatis dignissimos eaque impedit
+                explicabo harum, possimus, placeat deserunt quo sequi quis
+                doloremque. Maiores sequi voluptates vitae deserunt nam quasi
+                ipsam omnis assumenda commodi in sint, similique excepturi
+                incidunt provident sapiente nihil, dicta saepe. Sapiente magnam
+                voluptate similique mollitia placeat, rem voluptates explicabo
+                cupiditate commodi debitis, ducimus harum.
               </p>
             </div>
           </Tab.Panel>
